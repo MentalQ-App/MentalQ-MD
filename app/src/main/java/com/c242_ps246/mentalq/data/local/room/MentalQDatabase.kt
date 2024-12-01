10c90fe6a29336f9f8d65cb8abbb248ca5ad505f
valentinohrynt <hariyantovalentino@gmail.com>
2024-12-01 21:59:06 +0700

    feat(database): add chat entities to room database

--- app/src/main/java/com/c242_ps246/mentalq/data/local/room/MentalQDatabase.kt
+++ app/src/main/java/com/c242_ps246/mentalq/data/local/room/MentalQDatabase.kt
@@ -2,12 +2,14 @@ package com.c242_ps246.mentalq.data.local.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.c242_ps246.mentalq.data.remote.response.ChatMessageItem
+import com.c242_ps246.mentalq.data.remote.response.ChatRoomItem
 import com.c242_ps246.mentalq.data.remote.response.ListAnalysisItem
 import com.c242_ps246.mentalq.data.remote.response.ListNoteItem
 import com.c242_ps246.mentalq.data.remote.response.UserData
 
 @Database(
-    entities = [ListNoteItem::class, UserData::class, ListAnalysisItem::class],
+    entities = [ListNoteItem::class, UserData::class, ListAnalysisItem::class, ChatRoomItem::class, ChatMessageItem::class],
     version = 2,
     exportSchema = false
 )
