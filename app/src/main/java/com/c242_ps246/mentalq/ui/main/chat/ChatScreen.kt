d4a87cac98e5bf69164d8039b9fc081be8f5582f
valentinohrynt <hariyantovalentino@gmail.com>
2025-03-14 00:55:53 +0700

    feat: Update API endpoint, add "Not Available" string, and disable Google sign-in
    -   Updated the API endpoint in `build.gradle.kts` to `https://mentalq-backend.vercel.app/api/`.
    -   Added a new string resource "not_available" in `strings.xml` and `strings.xml (in)` for unavailable features.
    -   Disabled the Google sign-in button in `AuthScreen.kt` by setting `enabled` to `false` and adding "(Not Available)" text to the button.
    - Updated the title and subtitle message when chat room is empty in ChatScreen.kt

--- app/src/main/java/com/c242_ps246/mentalq/ui/main/chat/ChatScreen.kt
+++ app/src/main/java/com/c242_ps246/mentalq/ui/main/chat/ChatScreen.kt
@@ -3,13 +3,28 @@ package com.c242_ps246.mentalq.ui.main.chat
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -55,8 +70,10 @@ fun ChatScreen(
         ) {
             if (chatRooms.isEmpty() && !uiState.isLoading) {
                 EmptyState(
-                    title = stringResource(R.string.no_messages),
-                    subtitle = stringResource(R.string.no_messages_desc)
+//                    title = stringResource(R.string.no_messages),
+//                    subtitle = stringResource(R.string.no_messages_desc)
+                    title = "Maintenance",
+                    subtitle = "This feature is under maintenance"
                 )
             } else {
                 LazyColumn(
